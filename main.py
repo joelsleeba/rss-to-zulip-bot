6d6a8aeeaa62fd586dc085fac25087e243f1368e
joelsleeba <joelsleeba1@gmail.com>
2024-03-30 19:13:29 +0530

    small formatting fix when author field is empty

--- main.py
+++ main.py
@@ -13,25 +13,27 @@ ZULIP_STREAM_NAME = 'articles'
 
 # Define the dictionary of feed names and their RSS links
 RSS_FEEDS = {
-    "Dan Ma's Topology Blog" : "https://dantopology.wordpress.com/feed/",
-    "The Higher Geometer" : "https://thehighergeometer.wordpress.com/feed/",
-    "Terence Tao" : "https://terrytao.wordpress.com/feed/",
-    "Peter Woit" : "https://www.math.columbia.edu/~woit/wordpress/atom.xml",
-    "Math3ma" : "http://www.math3ma.com/blog/rss.xml",
-    "Math ∩ Programming" : "https://www.jeremykun.com/index.xml",
-    "DESVL" : "https://desvl.xyz/atom.xml",
-    "George Lowther" : "https://almostsuremath.com/feed",
-    "n-Category Cafe" : "https://golem.ph.utexas.edu/category/atom10.xml",
-    "Mathematics, Quanta Magazine" : "https://api.quantamagazine.org/mathematics/feed",
-    "Physics, Quanta Magazine" : "https://api.quantamagazine.org/physics/feed",
-    "Biology, Quanta Magazine" : "https://api.quantamagazine.org/biology/feed",
-    "Computer Science, Quanta Magazine" : "https://api.quantamagazine.org/computer-science/feed",
-    "Steven Wolfram, Writings" : "https://writings.stephenwolfram.com/feed",
-    "Bit Player" : "http://bit-player.org/feed",
-    "Azimuth" : "https://johncarlosbaez.wordpress.com/feed",
+    "Dan Ma's Topology Blog": "https://dantopology.wordpress.com/feed/",
+    "The Higher Geometer": "https://thehighergeometer.wordpress.com/feed/",
+    "Terence Tao": "https://terrytao.wordpress.com/feed/",
+    "Peter Woit": "https://www.math.columbia.edu/~woit/wordpress/atom.xml",
+    "Math3ma": "http://www.math3ma.com/blog/rss.xml",
+    "Math ∩ Programming": "https://www.jeremykun.com/index.xml",
+    "DESVL": "https://desvl.xyz/atom.xml",
+    "George Lowther": "https://almostsuremath.com/feed",
+    "n-Category Cafe": "https://golem.ph.utexas.edu/category/atom10.xml",
+    "Mathematics, Quanta Magazine": "https://api.quantamagazine.org/mathematics/feed",
+    "Physics, Quanta Magazine": "https://api.quantamagazine.org/physics/feed",
+    "Biology, Quanta Magazine": "https://api.quantamagazine.org/biology/feed",
+    "Computer Science, Quanta Magazine": "https://api.quantamagazine.org/computer-science/feed",
+    "Steven Wolfram, Writings": "https://writings.stephenwolfram.com/feed",
+    "Bit Player": "http://bit-player.org/feed",
+    "Azimuth": "https://johncarlosbaez.wordpress.com/feed",
 }
 
 # Function to send a message to Zulip
+
+
 def send_zulip_message(content, topic):
     client = zulip.Client(email=ZULIP_EMAIL, client='rss-feed-bot/0.1')
     data = {
@@ -43,6 +45,8 @@ def send_zulip_message(content, topic):
     client.send_message(data)
 
 # Get the url of the last article update sent to the stream
+
+
 def last_article_update_link(topic):
     client = zulip.Client(email=ZULIP_EMAIL, client='test-github-client/0.1')
     request: Dict[str, Any] = {
@@ -63,7 +67,8 @@ def last_article_update_link(topic):
             latest_message = messages[0]
             latest_message_content = latest_message["content"]
             url_pattern = r'(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)'
-            latest_arxiv_link = re.findall(url_pattern, latest_message_content)[0]
+            latest_arxiv_link = re.findall(
+                url_pattern, latest_message_content)[0]
             return latest_arxiv_link
         else:
             return None
@@ -72,26 +77,34 @@ def last_article_update_link(topic):
         return None
 
 # Function to fetch latest articles from arXiv
+
+
 def update_zulip_stream():
-    for  feed_name, feed_url in RSS_FEEDS.items():
+    for feed_name, feed_url in RSS_FEEDS.items():
         feed = feedparser.parse(feed_url)
         topic = feed.feed.title
         last_updated_article_link = last_article_update_link(topic)
-        flag = True if last_updated_article_link else False # To avoid the case when not having an article ends up not getting to the execution part of the for loop
-        for article in feed.entries[:4][::-1]: #This is to get the latest 5 articles in ascending order by time
+        # To avoid the case when not having an article ends up not getting to the execution part of the for loop
+        flag = True if last_updated_article_link else False
+        # This is to get the latest 5 articles in ascending order by time
+        for article in feed.entries[:4][::-1]:
             print(feed_name)
             link = article.link
             if flag:
                 if link == last_updated_article_link:
                     flag = False
                 continue
-            title = markdownify.markdownify(article.title.replace('$^{\\ast}$', '* ').replace('$^*$', '* ').replace('$^*$', '* ').replace("$", "$$").replace("\n", " "))
+            title = markdownify.markdownify(article.title.replace('$^{\\ast}$', '* ').replace(
+                '$^*$', '* ').replace('$^*$', '* ').replace("$", "$$").replace("\n", " "))
             published = time.strftime("%d %B %Y", article.published_parsed)
-            author = article.author if 'author' in article.keys() else "" # Some rss feeds lack these fields
+            # Some rss feeds lack these fields
+            author = article.author+", " if 'author' in article.keys() else ""
             summary = markdownify.markdownify(article.description)
-            tags = ", ".join([entry['term'] for entry in article.tags]) if 'tags' in article else "" # Some feeds lack tags
-            message = f"\n**[{title}]({link})**\n*{author}, {published}*\n\n{summary}\n\n*{tags}*"
+            tags = ", ".join([entry['term'] for entry in article.tags]
+                             ) if 'tags' in article else ""  # Some feeds lack tags
+            message = f"\n**[{title}]({link})**\n*{author}{published}*\n\n{summary}\n\n*{tags}*"
             send_zulip_message(message, topic)
+
 
 # Main function to check for new articles periodically
 if __name__ == "__main__":
